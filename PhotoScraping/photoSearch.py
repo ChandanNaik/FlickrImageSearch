fed19c753dc24d4381745ac17856883a1fc573ca
Thanika Reddy <thanika.reddy@gmail.com>
2019-03-27 18:55:30 -0600

    Using the Flickr API search method to get all photos for a search phrase. Response format : JSON. Use IDs of images in response to construct URL of each image.

---
+++ PhotoScraping/photoSearch.py
@@ -0,0 +1,24 @@
+import urllib.parse
+import requests
+import webbrowser
+
+#Keywords input
+searchKeywords = input('Enter search keywords: ')
+
+#Construct URL
+searchApi = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ad31c06a51ac878377e46684228537aa&format=json&nojsoncallback=1&'
+searchUrl = searchApi+urllib.parse.urlencode({'text':searchKeywords})
+
+#Get response , convert to JSON
+searchResultJson = requests.get(searchUrl).json()
+photosData = searchResultJson["photos"]["photo"]
+
+#Print image IDs
+count = 0
+for photoData in photosData:
+	print(photoData["id"])
+	count+=1
+	#Open every 20th image in webbrowser
+	if count%20 == 0:
+		imageUrl = 'https://farm'+str(photoData["farm"])+'.staticflickr.com/'+photoData["server"]+'/'+photoData["id"]+'_'+photoData["secret"]+'.jpg'
+		webbrowser.open(imageUrl)
