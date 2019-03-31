bc52272ab4132a908f148e62c7b3d259681245eb
Thanika Reddy <thanika.reddy@gmail.com>
2019-03-30 18:21:31 -0600

    Storing ID, filename and tags of each image that is dumped, in JSON

--- PhotoScraping/dumpToBucket.py
+++ PhotoScraping/dumpToBucket.py
@@ -3,10 +3,13 @@
 #Author: thanika							   //
 #////////////////////////////////////////////////
 
+import csv
+import json
 import boto3
 import datetime
 import flickrapi
 import urllib.request
+from collections import defaultdict
 
 #Create S3 Bucket
 s3_resource = boto3.resource('s3')
@@ -17,11 +20,15 @@ key='72867a4388924cd9840ae813f23a70cf'
 secret='49021d0404efb5c3'
 flickr = flickrapi.FlickrAPI(key,secret, format='parsed-json')
 
+#Dictionary to store image path in S3 and its tags
+#{"id1": {"filename", "tag1", "tag2", "tag3"}, "id2": {"filename", "tag1", "tag2", "tag3"}, "id3": {"filename", "tag1", "tag2", "tag3"}}
+image_dictionary = defaultdict(dict)
+
 #Specify start and end dates. 
 #For each date, 100 images are obtained by default. To change, vary "per_page".
 #Around 200 days for 100k photos if 500 photos per day
 #From date
-date1 = '2019-03-01' 
+date1 = '2019-03-20' 
 #To date
 date2 = '2019-03-29' 
 start = datetime.datetime.strptime(date1, '%Y-%m-%d')
@@ -44,6 +51,19 @@ while start <= end:
 		
 		try:
 			urllib.request.urlretrieve(imageURL, 'temporary.jpg')
+
+			#ML object detection here - gives three tags for the image
+			#/////////////////////////////////////////////////////////
+
+			#If tags are obtained, then store in dictionary and upload
+
+			#Store in dictionary
+			image_dictionary[photo["id"]]["filename"] = photo["id"]+'.jpg'
+			image_dictionary[photo["id"]]["tag1"] = 'TAG 1'
+			image_dictionary[photo["id"]]["tag2"] = 'TAG 2'
+			image_dictionary[photo["id"]]["tag3"] = 'TAG 3'
+
+			#Upload image to S3 Bucket
 			our_bucket.upload_file(Filename='temporary.jpg', Key=photo["id"]+'.jpg')
 			print("SUCCESSfully dumped photo with ID: "+photo["id"])
 
@@ -51,5 +71,8 @@ while start <= end:
 			print("FAILED to dump photo with ID: "+photo["id"])
 
 	start += step
+
+#Dump into json file
+json.dump(image_dictionary, open("image_dictionary.json","w"))
 
 #our_bucket.download_file('testing.jpg','testing_download.jpg')
