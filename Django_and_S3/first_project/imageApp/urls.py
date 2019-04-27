10ef5dd92c6f991df2f3b6beb8a372f1c6c22fc7
Thanika Reddy <thanika.reddy@gmail.com>
2019-04-27 10:56:43 -0600

    URLs redirect file for image search app

---
+++ Django_and_S3/first_project/imageApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+urlpatterns = [
+    path('', views.search, name='imageApp-home'),
+    path('searchResults/', views.searchResults, name='imageApp-searchResults'),
+    path('dumpToBucket/', views.dumpToBucket, name='imageApp-dumpToBucket'),
+    path('performDump/', views.performDump, name='imageApp-performDump'),
+    path('testDB/', views.testDB, name='imageApp-testDB')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
