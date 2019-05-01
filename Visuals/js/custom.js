ce808b0597280ed06bc6807cb280b278d4aa2a3c
Chandan_Naik <chandan94nagarajnaik@gmail.com>
2019-05-01 13:56:34 -0600

    Changes to visuals: User can upload an image and the uploaded image will be displayed on screen; Added search button

--- Visuals/js/custom.js
+++ Visuals/js/custom.js
@@ -5,7 +5,7 @@
   -------------------------------------------------------------------------------*/
 
   $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(1000); // set duration in brackets
   });
 
 
@@ -22,17 +22,33 @@
 
   /* Back top
   -----------------------------------------------*/
-  
+
   $(window).scroll(function() {
         if ($(this).scrollTop() > 200) {
         $('.go-top').fadeIn(200);
         } else {
           $('.go-top').fadeOut(200);
         }
-        });   
+        });
         // Animate the scroll to top
       $('.go-top').click(function(event) {
         event.preventDefault();
       $('html, body').animate({scrollTop: 0}, 300);
       })
 
+  /* Upload Image
+  -----------------------------------------------*/
+
+  $('.file-input').change(function(){
+      var curElement = $(this).parent().parent().parent().find('.image');
+      console.log(curElement);
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          // get loaded data and render thumbnail.
+          curElement.attr('src', e.target.result);
+      };
+
+      // read the image file as a data URL.
+      reader.readAsDataURL(this.files[0]);
+  });
